2dccd216ac08df2ac848adf9b55790f9126099a0
Piotr Chmolowski <piotr@chmolowski.pl>
2016-02-18 22:17:52 +0100

    Keep body in a separate col, add tags & parent

--- db/migrate/20150831131759_create_wellspring_entries.wellspring.rb
+++ db/migrate/20150831131759_create_wellspring_entries.wellspring.rb
@@ -4,12 +4,18 @@ class CreateWellspringEntries < ActiveRecord::Migration
       t.string :type, index: true
       t.string :title
       t.string :slug, index: true
+      t.text :body
       t.json :payload
       t.integer :user_id, index: true
       t.string :author_name
       t.datetime :published_at
+      t.boolean :draft, default: true, index: true
+      t.text :tags, array: true, default: []
+      t.integer :parent_id, index: true
 
       t.timestamps null: false
     end
+
+    add_index :wellspring_entries, :tags, using: 'gin'
   end
 end
