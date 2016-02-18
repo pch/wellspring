5b226ae62fe2e8490425560b2c95c9c526b2f2c9
Piotr Chmolowski <piotr@chmolowski.pl>
2016-02-18 22:29:59 +0100

    Navigation, body class helper, layout markup

--- app/helpers/wellspring/application_helper.rb
+++ app/helpers/wellspring/application_helper.rb
@@ -1,4 +1,14 @@
 module Wellspring
   module ApplicationHelper
+    def body_class(options = {})
+      controller_name = controller.controller_path.gsub('/','-')
+      basic_body_class = "#{controller_name} #{controller_name}-#{controller.action_name}"
+
+      if content_for?(:body_class)
+        [basic_body_class, content_for(:body_class)].join(' ')
+      else
+        basic_body_class
+      end
+    end
   end
 end
