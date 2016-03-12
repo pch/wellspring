1a897aa1f673b76c0eda79e86819d1f2974ccd4f
Piotr Chmolowski <piotr@chmolowski.pl>
2016-03-12 16:45:11 +0100

    Remove previews controller

--- app/controllers/wellspring/previews_controller.rb
+++
@@ -1,9 +0,0 @@
-require_dependency "wellspring/application_controller"
-
-module Wellspring
-  class PreviewsController < ApplicationController
-    def show
-      render layout: false
-    end
-  end
-end
